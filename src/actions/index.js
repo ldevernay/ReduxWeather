79b3d1e1f0748959026e2ec666f89115e85da30a
ldevernay <boblehamster@yahoo.fr>
2017-12-05 09:39:41 +0100

    Redux bind action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,7 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const URL = `${ROOT_URL}q=${city},us`;
-    const request = axios.get(url);
+    const request = axios.get(URL);
 
 return {
     type: FETCH_WEATHER,
