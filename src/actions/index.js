88b0b4e8dddbeb6839b2bcc0fbd67a0ae9400c9f
ldevernay <boblehamster@yahoo.fr>
2017-12-05 10:07:17 +0100

    Get promise, extract data through redux-promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 const API_KEY = '01dc2db78749e5d5a3ea2479ee8dfb5b';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-    const URL = `${ROOT_URL}q=${city},us`;
+    const URL = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(URL);
 
-return {
-    type: FETCH_WEATHER,
-    payload: request
-};
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
 }
